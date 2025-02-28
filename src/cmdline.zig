67ebf98b815a4fdf3be0221d8320ead4b134b79e
Riley Fischer <rfmineguy@gmail.com>
2025-02-27 18:01:56 -0800

    Cmdline: Template name is now nullible, must use .?

--- src/cmdline.zig
+++ src/cmdline.zig
@@ -9,7 +9,7 @@ pub fn create_adt_command(app: *yazap.App, t: template.Template, alloc: std.mem.
     for (t.args.items) |arg| {
         // NOTE: Memory leaks?
         const desc = if (arg.def) |d| try std.fmt.allocPrint(alloc, "default = {s}", .{d}) else "";
-        try command.addArg(Arg.singleValueOption(arg.name, arg.name[0], desc));
+        try command.addArg(Arg.singleValueOption(arg.name.?, arg.name.?[0], desc));
     }
 
     try command.addArg(Arg.singleValueOption("outputdir", 'o', "default = ."));
