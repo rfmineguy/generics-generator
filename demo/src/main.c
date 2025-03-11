53cf1ed29c41e2ca95cf3970ed9e2970e4208624
Riley Fischer <rfmineguy@gmail.com>
2025-03-11 14:57:24 -0700

    Demo: Finish main for demonstration

--- demo/src/main.c
+++ demo/src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "generated/linked_list_vec2.h"
 #include "generated/linked_list_int.h"
+#include "generated/binary_tree_vec2.h"
 #include "generated/binary_tree_int.h"
 
 int main() {
@@ -25,29 +26,57 @@ int main() {
 
 
 	printf("=======================\n");
-	printf("     Binary tree\n");
+	printf("     Binary tree int\n");
 	printf("=======================\n");
-	binary_tree_int bt = bt_int_new();
-	bt_int_insert(&bt, 9);
-	bt_int_insert(&bt, 4);
-	bt_int_insert(&bt, 1);
-	bt_int_insert(&bt, 6);
-	bt_int_insert(&bt, 3);
-	bt_int_insert(&bt, 20);
-	bt_int_print(&bt);
+	binary_tree_int bt1 = bt_int_new();
+	bt_int_insert(&bt1, 9);
+	bt_int_insert(&bt1, 4);
+	bt_int_insert(&bt1, 1);
+	bt_int_insert(&bt1, 6);
+	bt_int_insert(&bt1, 3);
+	bt_int_insert(&bt1, 20);
+	bt_int_print(&bt1);
 	printf("-=-----=-\n");
-	bt_int_delete(&bt, 9);
-	bt_int_print(&bt);
+	bt_int_delete(&bt1, 9);
+	bt_int_print(&bt1);
 	printf("-=-----=-\n");
-	bt_int_delete(&bt, 4);
-	bt_int_print(&bt);
+	bt_int_delete(&bt1, 4);
+	bt_int_print(&bt1);
 	printf("-=-----=-\n");
-	bt_int_delete(&bt, 3);
-	bt_int_print(&bt);
+	bt_int_delete(&bt1, 3);
+	bt_int_print(&bt1);
 
-	if (bt_int_find(&bt, 3)) {
+	if (bt_int_find(&bt1, 3)) {
 		printf("Found 4\n");
 	}
 
-	bt_int_free(&bt);
+	bt_int_free(&bt1);
+
+
+	printf("=======================\n");
+	printf("     Binary tree vec2\n");
+	printf("=======================\n");
+	binary_tree_vec2 bt2 = bt_vec2_new();
+	bt_vec2_insert(&bt2, (vec2){0, 0});
+	bt_vec2_insert(&bt2, (vec2){1, 1});
+	bt_vec2_insert(&bt2, (vec2){3, 3});
+	bt_vec2_insert(&bt2, (vec2){2, 2});
+	bt_vec2_insert(&bt2, (vec2){4, 4});
+	bt_vec2_insert(&bt2, (vec2){5, 5});
+	bt_vec2_print(&bt2);
+	printf("-=-----=-\n");
+	bt_vec2_delete(&bt2, (vec2){0, 0});
+	bt_vec2_print(&bt2);
+	printf("-=-----=-\n");
+	bt_vec2_delete(&bt2, (vec2){4, 4});
+	bt_vec2_print(&bt2);
+	printf("-=-----=-\n");
+	bt_vec2_delete(&bt2, (vec2){5, 5});
+	bt_vec2_print(&bt2);
+
+	if (bt_vec2_find(&bt2, (vec2){1, 1})) {
+		printf("Found {1,1}\n");
+	}
+
+	bt_vec2_free(&bt2);
 }
