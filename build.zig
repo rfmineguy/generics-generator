684539b911e775d4f81e8996c193992f6b3097f1
Riley Fischer <rfmineguy@gmail.com>
2025-04-03 12:14:50 -0700

    0.14 : Upgrade to zig 0.14 and fix build problems with the libraries

--- build.zig
+++ build.zig
@@ -36,12 +36,14 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const toml_dep = b.dependency("zig_toml", .{});
-    exe.root_module.addImport("zig_toml", toml_dep.module("toml"));
-    const known_folders = b.dependency("known-folders", .{}).module("known-folders");
-    exe.root_module.addImport("known-folders", known_folders);
-    const yazap = b.dependency("yazap", .{});
-    exe.root_module.addImport("yazap", yazap.module("yazap"));
+    const toml_dep = b.dependency("toml", .{}).module("zig-toml");
+    exe.root_module.addImport("toml", toml_dep);
+
+    const known_folders = b.dependency("knownfolders", .{}).module("known-folders");
+    exe.root_module.addImport("knownfolders", known_folders);
+
+    const yazap = b.dependency("yazap", .{}).module("yazap");
+    exe.root_module.addImport("yazap", yazap);
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
